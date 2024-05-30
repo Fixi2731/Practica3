4da8e75da388e3f520ac11715da615c85f5c057e
Fixi2731 <322011346@alumno.enp.unam.mx>
2024-05-30 13:33:59 -0600

    Practica 3 :)

--- P3_ejecuta.sh
+++ P3_ejecuta.sh
@@ -4,9 +4,8 @@ action=$1
 pass=false
 date=$(date +%d/%m/%Y)
 time=$(date +%H%M)
-filePath=false
-directoryPath=false
-
+fileRegPath=false
+directoryRegPath=false
 
 if [[ -n $action && -n $2 ]]; then 
 	echo "Bienvenid@!"
@@ -17,47 +16,53 @@ else
 fi
 
 if [ $1 = "recuperacion" ]; then
-	if [[ $2 =~ *.tar ]]; then
+	if [[ $2 =~ \.tar$ ]]; then
 		filePath=$(find / -type f -name $2)
-	else 	
-		filePath=$(find / -type f -name "$2.tar")
+	else 
+		file=$2	
+		filePath=$(find / -type f -name ${file}.tar)
 	fi
+	fileRegPath=true
+
 elif [ $1 = "respaldo" ]; then
-	file=$2
-	directoryName=${file%/}
+	direct=$2
+	directoryName=${direct%/}
 	directoryPath=$(find / -type d -name $directoryName)
+	directoryRegPath=true
 else
 	echo "Accion no encontrada:recuperar/respaldo" 
 	exit 0
 fi
 
 respaldo() {
-	tar -cvf respaldo_$(date +%d%m%Y)-$time.tar
-	echo -e "Respaldo del directorio : $directoryPath \nElaborado el $date a las $(date +%R)"
+	fileName=respaldo_$(date +%d%m%Y)-$time.tar
+	tar -cvf $fileName $directoryPath
+	echo -e "Respaldo del directorio : $directoryPath \nElaborado el $date a las $(date +%R) \nGuardado como: $fileName"
+	exit 0
 }
 
 recuperacion() {
 	directoryName=rec$(date +%F)
 	mkdir $directoryName
 	tar -xvf $filePath -C $directoryName
+	echo -e "Descompresion del archivo : $filePath \nRealizada el $date a las $(date +%R) \nGuardado en: $directoryName"
+	exit 0
 }
 
-if $directoryPath; then 
-	respaldo
-	exit 0
-else
-	echo "Nombre del directorio invalido:Directorio no encontrado"
-	echo $directoryPath
-	exit 0
-fi
-
-if $filePath; then 
-	recuperacion
-	echo -e "Descompresion del archivo : $filePath \nRealizada el $date a las $(date +%R)"
-	exit 0
-else
-	$(find / -type d -name $2) && echo La ruta proporcionada es un directorio || echo El archivo proporcionado es invalido
+if $directoryRegPath ; then
+	if [ -n $directoryPath ] ; then 
+		respaldo
+	else
+		echo "Nombre del directorio invalido:Directorio no encontrado"
+	fi
 	exit 0
 fi
 
-
+if $fileRegPath ; then
+	if [ -n $filePath ]; then 
+		recuperacion
+	else
+		$(find / -type d -name $2) && echo El nombre del archivo proporcionado es un directorio || echo El archivo proporcionado es invalido
+	fi
+	exit 0
+fi
